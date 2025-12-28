df655efc26c087ca86bdd359e2b6fa999cc43896
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2025-12-28 15:01:56 -0300

    Adcionei a ação do botão

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
-// Atribuindo as variáveis
-let gerador = Math.floor(Math.random()*5)
 let fundo = document.querySelector("body")
 let visualizar = document.querySelector("span")
-// Teste no console pra ver se esta batendo
-console.log(gerador)
-// Listas com os códigos e nomes
+let botao = document.querySelector("button")
+
+function geradorNumero() {
+    const gerador = Math.floor(Math.random()*5)
+    console.log(gerador)
+    return gerador
+}
+
 let coresCodigo = ["#d2d", "#df4", "#5d7", "#a43", "#78f",]
 let coresNome = ["Rosa","Amarelo","Verde","Carmesin","Roxo"]
-// Ação de mudar a cor
-fundo.style.backgroundColor = (coresCodigo[gerador])
 
-visualizar.textContent = coresNome[gerador]
+botao.addEventListener("click", function(){
+    let numero = geradorNumero()
+    fundo.style.backgroundColor = coresCodigo[numero]
+    visualizar.textContent = coresNome[numero]
+})
