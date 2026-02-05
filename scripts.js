9e83d08b51a36f805593db17fde77d1a303f1c81
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-02-05 08:39:26 -0300

    Mudanças no design

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 let fundo = document.querySelector("body")
 let visualizar = document.querySelector("span")
 let botao = document.querySelector("button")
+let botaoNomes = document.querySelector("#nomes")
+let botaoHexadecimal = document.querySelector("#hexadecimal")
+
+let tipoEscrita = 0
+
+
+
+
+
 
 function geradorNumero() {
     const gerador = Math.floor(Math.random()*5)
@@ -8,11 +17,32 @@ function geradorNumero() {
     return gerador
 }
 
+
 let coresCodigo = ["#d2d", "#df4", "#5d7", "#a43", "#78f",]
 let coresNome = ["Rosa","Amarelo","Verde","Carmesin","Roxo"]
+
+
+
+botaoNomes.addEventListener("click", function(event){
+    event.preventDefault()
+    tipoEscrita = 0
+})
+
+botaoHexadecimal.addEventListener("click", function(event){
+    event.preventDefault()
+    tipoEscrita = 1
+})
+
+
+
 
 botao.addEventListener("click", function(){
     let numero = geradorNumero()
     fundo.style.backgroundColor = coresCodigo[numero]
-    visualizar.textContent = coresNome[numero]
+    if(tipoEscrita === 0){
+        visualizar.textContent = coresNome[numero]
+    }
+    else{
+        visualizar.textContent = coresCodigo[numero]
+    } 
 })
